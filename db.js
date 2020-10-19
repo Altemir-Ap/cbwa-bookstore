db78279a9e38beec147d9016fbc28c7bd75a8a12
Altemir Almeida <altemir.git@gmail.com>
2020-10-19 20:56:07 +0100

    Fixing deprecated mongo option

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const uri = process.env.MONGO_URI;
 const MongoClient = require('mongodb').MongoClient;
 const DB_NAME = 'book-store';
-
+const MONGO_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 module.exports = () => {
   const get = (collectionName) => {
     return new Promise((resolve, reject) => {
-      MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+      MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
         const db = client.db(DB_NAME);
         const collection = db.collection(collectionName);
         collection.find({}).toArray((err, docs) => {
@@ -18,7 +18,7 @@ module.exports = () => {
 
   const add = (collectionName, item) => {
     return new Promise((resolve, reject) => {
-      MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+      MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
         const db = client.db(DB_NAME);
         const collection = db.collection(collectionName);
 
@@ -31,7 +31,7 @@ module.exports = () => {
 
   const count = (collectionName) => {
     return new Promise((resolve, reject) => {
-      MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+      MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
         const db = client.db(DB_NAME);
         const collection = db.collection(collectionName);
 
