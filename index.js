da233439bada454685999b3f320e8cd811e6340c
Altemir Almeida <altemir.git@gmail.com>
2020-10-21 18:22:08 +0100

    Implementing shared key authentication

--- index.js
+++ index.js
@@ -5,12 +5,48 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 const authorController = require('./controllers/authors')();
 const booksController = require('./controllers/books')();
+
+const users = require('./models/users')();
 const app = (module.exports = express());
 
 app.use('/', express.static('static'));
 
 app.use((req, res, next) => {
   console.log('[%s] %s -- %s', new Date(), req.method, req.url);
+  next();
+});
+app.use(async (req, res, next) => {
+  const FailedAuthMessage = {
+    error: 'Failed authentication',
+    message: 'Go away',
+    code: 'xxx',
+  };
+
+  const suppliedKey = req.headers['x-api-key'];
+  const clientIp =
+    req.headers['x-fowarded-for'] || req.connection.remoteAddress;
+  if (!suppliedKey) {
+    console.log(
+      '[%s] FAILED AUTHENTICATION -- %s, No Key Supplied',
+      new Date(),
+      clientIp,
+    );
+    FailedAuthMessage.code = '01';
+    return res.status(401).json(FailedAuthMessage);
+  }
+
+  const user = await users.getByKey(suppliedKey);
+
+  if (!user) {
+    console.log(
+      '[%s] FAILED AUTHENTICATION -- %s, BAD Key Supplied',
+      new Date(),
+      clientIp,
+    );
+
+    FailedAuthMessage.code = '02';
+    return res.status(401).json(FailedAuthMessage);
+  }
   next();
 });
 
